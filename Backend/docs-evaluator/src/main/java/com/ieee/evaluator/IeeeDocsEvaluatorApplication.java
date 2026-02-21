b42031e26bc2bc70245d4e93c6d73a303c71379b
Nathan Xander Lada <nlada27@gmail.com>
2026-02-21 17:09:50 +0800

    feat: init capstone backend engine with document parsers

---
+++ Backend/docs-evaluator/src/main/java/com/ieee/evaluator/IeeeDocsEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.ieee.evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeeeDocsEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeeeDocsEvaluatorApplication.class, args);
+	}
+
+}
